8026632211f636ee6ede6853e154b44f0edaff1c
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-07-21 14:15:45 +0200

    read user homedir and do stuff

--- sketch.js
+++ sketch.js
@@ -1,16 +1,35 @@
+const fs = require("fs");
+
+var userHome = getUserHome();
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(50);
+
+  fs.readdir(userHome,
+    function(err, data) {
+      for (var i = 0; i < data.length; i++) {
+        fill(random(60,255))
+        rect(random(0,windowWidth),random(0,windowHeight),100,100)
+      }
+    }
+  );
+
+  noLoop();
+}
+
+function mouseClicked() {
+  loop();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
