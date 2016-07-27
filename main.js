b2633cc7e9b4ca8933646371ebfea3a43dd5df65
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-07-27 18:53:43 +0200

    implement basic live-reload

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const electron = require("electron");
 const {app, BrowserWindow} = electron;
+const fs = require("fs");
 
 app.on("ready", () => {
+  // load sketch window
   var win = new BrowserWindow({width: 640, height: 480});
-  win.loadURL("file://" + __dirname + "/index.html");
+  win.loadURL("file://" + __dirname + "/sketch/index.html");
+
+  // auto-reload on filechange in sketch folder
+  fs.watch(__dirname + "/sketch", () =>{
+    win.reload();
+  });
 
 });
